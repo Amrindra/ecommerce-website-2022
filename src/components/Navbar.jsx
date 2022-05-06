a3c75feca5820f3aa2da64b0c824f9abf459f18a
Amrindra Ouk <amrindraouk@Amrindras-MacBook-Pro.local>
2022-05-06 15:29:00 -0400

    added flex box to navabr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import styled from "styled-components";
 
 function Navbar() {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <LeftSide>Left</LeftSide>
+        <Center>Center</Center>
+        <RightSide>RIght</RightSide>
+      </Wrapper>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const LeftSide = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const RightSide = styled.div`
+  flex: 1;
 `;
 
 export default Navbar;
