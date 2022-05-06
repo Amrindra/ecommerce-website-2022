d1c15c8016fc0820080d2f66ee5d00e0ef423609
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-06 19:53:09 -0400

    Designed Navbar added some icons to it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingBasketOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -5,9 +7,27 @@ function Navbar() {
   return (
     <Container>
       <Wrapper>
-        <LeftSide>Left</LeftSide>
-        <Center>Center</Center>
-        <RightSide>RIght</RightSide>
+        <LeftSide>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </LeftSide>
+
+        <Center>
+          <Logo>SHOP</Logo>
+        </Center>
+
+        <RightSide>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingBasketOutlined />
+            </Badge>
+          </MenuItem>
+        </RightSide>
       </Wrapper>
     </Container>
   );
@@ -20,17 +40,52 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
 const LeftSide = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 const RightSide = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 export default Navbar;
