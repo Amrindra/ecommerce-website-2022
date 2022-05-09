6670f2953fead8d770e0d8da2d38c0c7c829071b
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-09 13:29:17 -0400

    created ProductItem and Products components and then designed these components

---
+++ src/components/Products.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import ProductItem from "./ProductItem";
+
+const Products = () => {
+  return (
+    <Container>
+      {popularProducts.map((item) => (
+        <ProductItem item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export default Products;
