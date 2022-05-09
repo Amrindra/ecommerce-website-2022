9e9e0a386ad626a0777e3a5874f90c1ef12ba1a7
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-09 11:58:39 -0400

    passed props to CategoryItem

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,21 @@
 import styled from "styled-components";
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
 
 const Categories = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem item={item} />
+      ))}
+    </Container>
+  );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
 
 export default Categories;
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,9 +1,21 @@
 import styled from "styled-components";
 
-const CategoryItem = () => {
-  return <Container></Container>;
+const CategoryItem = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
 };
 
 const Container = styled.div``;
+const Image = styled.img``;
+const Info = styled.div``;
+const Title = styled.div``;
+const Button = styled.button``;
 
 export default CategoryItem;
