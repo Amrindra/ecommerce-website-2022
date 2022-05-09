085a1fc67b2eb479e0d2a9517c30d57d6bf8e23a
Amrindra Ouk <amrindraouk@Amrindras-MacBook-Pro.local>
2022-05-09 15:38:08 -0400

    added some select option to Productlist component

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -15,11 +15,37 @@ function ProductList() {
 
       <FilterContain>
         <Filter>
-          <FilterText>Filter Products: </FilterText>
+          <FilterText>Filter Products: </FilterText>{" "}
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
         </Filter>
 
         <Filter>
           <FilterText>Sort Products: </FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
         </Filter>
       </FilterContain>
 
@@ -46,12 +72,14 @@ const Filter = styled.div`
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
-  margin-right: 20px;
+  margin-right: 15px;
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
 `;
+
+const Option = styled.option``;
 
 export default ProductList;
