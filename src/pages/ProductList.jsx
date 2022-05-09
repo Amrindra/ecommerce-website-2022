2fd03d5379f5e07221d795ae7cb10c46d5cecad9
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-09 14:53:43 -0400

    created productList component

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,57 @@
 import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
 
 function ProductList() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+
+      <Title>Dresses</Title>
+
+      <FilterContain>
+        <Filter>
+          <FilterText>Filter Products: </FilterText>
+        </Filter>
+
+        <Filter>
+          <FilterText>Sort Products: </FilterText>
+        </Filter>
+      </FilterContain>
+
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
 }
 
 const Container = styled.div``;
+
+const Title = styled.h1``;
+
+const FilterContain = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
 
 export default ProductList;
